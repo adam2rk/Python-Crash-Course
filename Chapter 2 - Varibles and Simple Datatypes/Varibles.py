f9509189efea58a5e9fe5ded536acda448883c77
Adam2rk <rehayilmazmm@outlook.com>
2025-12-10 20:49:04 -0800

    Chapter 2

---
+++ Chapter 2 - Varibles and Simple Datatypes/Varibles.py
@@ -0,0 +1,14 @@
+#This is a comment
+'''
+This varible x is a string
+Varibles in python is a reference to a value stored in memory
+'''
+#Python is dynamically types so you don't explicitly say the data type for example
+x = "Hello World"
+print(x)
+#This Varible y is a float
+y = 3.14
+print(y)
+#And now it's a string
+y = "Hello World 2"
+print(y)
