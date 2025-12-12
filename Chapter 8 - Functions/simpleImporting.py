9c8b43c0d81bde86a66fb6873524e4e15f850f71
Adam2rk <rehayilmazmm@outlook.com>
2025-12-11 21:08:09 -0800

    pizza

--- Chapter 8 - Functions/simpleImporting.py
+++ Chapter 8 - Functions/simpleImporting.py
@@ -2,3 +2,13 @@ import pizza
 
 pizza.make_pizza(16, "Extra Cheese")
 pizza.make_pizza(12, "Shrooms")
+
+#importing specfic functions
+#from module_name import function_name
+
+#or
+
+#importing all functions from a module
+#from pizza import *
+#then we could have done
+#make_pizza(16, "Extra cheese")
