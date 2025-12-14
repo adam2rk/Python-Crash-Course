814cbe2341f466c9e35700c3271efc1334af0f81
Adam2rk <rehayilmazmm@outlook.com>
2025-12-13 16:25:54 -0800

    skipped

---
+++ Chapter 12 - A Ship that fires bullets/start.py
@@ -0,0 +1,6 @@
+#Let's build a game called Alien invasion! we'll use a pygame a collection of fun powderful
+#Python modules that manage graphics, animation and even sound making it easier
+#for you to build sophisticated games
+
+#So pygame isn't up to date with my version of python so im skipping chapters 
+#12, 13, 14
